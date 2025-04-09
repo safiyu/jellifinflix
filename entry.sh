98fd4f6944f6be54e61e474686ba6d333feb7bfc
safiyu <dheensafiyu@gmail.com>
2025-04-09 13:51:45 +0200

    added additional checks

--- entry.sh
+++ entry.sh
@@ -2,7 +2,9 @@
 echo "Start: $(date)"
 echo "Starting scan"
 cp /app/replace.sh /media
+ls -l
 cd /media
+ls -l
 chmod +x replace.sh
 source replace.sh
 rm /media/replace.sh
--- replace.sh
+++ replace.sh
@@ -16,15 +16,17 @@ find . -type d -exec bash -c '
 
                 if [[ "$subdircount" -eq 1 ]]
                 then
-                    echo "$dir - No video file exists. creating $full_path"
-                    touch "$full_path"
+					if [ -f "$full_path" ]; then
+					  echo "$dir is upto date."
+					else
+					  echo "$dir - No video file exists. creating $full_path"
+					  touch "$full_path"
+					fi					
                 else
                     if [ -f "$full_path" ]; then
                         rm "$full_path"
                     fi
-                fi
-                
-                
+                fi 
             fi
             FILES_FOUND=$(find "$dir" -type f \( -name "*.mkv" -o -name "*.mp4" \) -print)
 	    	if [ -z "$FILES_FOUND" ]; then
@@ -37,4 +39,7 @@ find . -type d -exec bash -c '
 		fi
         done' bash {} +
 
-curl -s -S -d "" -w "jellyfin library refresh completed with http_code:%{http_code}\\n" -H "Accept: application/json" "$JF_URL/library/refresh?api_key=$JF_API_KEY"
+jfurl="$JF_URL/library/refresh?api_key=$JF_API_KEY"
+echo "Calling jellyfin endpoint: $jfurl"
+
+curl -s -S -d "" -w "jellyfin library refresh completed with http_code:%{http_code}\\n" -H "Accept: application/json" "$jfurl"
